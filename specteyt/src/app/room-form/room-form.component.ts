d9efc4bcde0c5904fd401880120f0cc8feab3ce2
Kevin Lim <klim560@gmail.com>
2018-12-25 23:22:09 -0800

    simplified roomId gen for cleaner urls

--- specteyt/src/app/room-form/room-form.component.ts
+++ specteyt/src/app/room-form/room-form.component.ts
@@ -16,23 +16,13 @@ export class RoomFormComponent {
     private roomService: RoomService
   ) {}
 
-  create_UUID(){
-      var dt = new Date().getTime();
-      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = (dt + Math.random()*16)%16 | 0;
-          dt = Math.floor(dt/16);
-          return (c=='x' ? r :(r&0x3|0x8)).toString(16);
-      });
-      return uuid;
-  }
-
   onSubmit() {
     //console.log(JSON.stringify(this.model));
     var roomName = this.model.roomName;
     var displayName = this.model.displayName;
 
-    // make a new room
-    var roomId = this.create_UUID();
+    // make a new room id with radix 36
+    var roomId = Math.random().toString(36).slice(2);
 
     this.addRoom(new Room(roomId, roomName));
     this.router.navigate([roomId]);
